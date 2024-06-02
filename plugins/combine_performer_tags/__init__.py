21fc4d98500bbd7072044187409af56fbe77999d
Bob Swift <bswift@rsds.ca>
2024-06-02 14:44:59 -0600

    Add option settings page.  General code cleanup.

--- plugins/combine_performer_tags/__init__.py
+++ plugins/combine_performer_tags/__init__.py
@@ -24,8 +24,18 @@
 # pylint: disable=import-error
 
 
-from picard import log
+from picard import (
+    config,
+    log,
+)
 from picard.metadata import register_track_metadata_processor
+from picard.plugins.combine_performer_tags.ui_options_combine_performer_tags import (
+    Ui_CombinePerformerTagsOptionsPage,
+)
+from picard.ui.options import (
+    OptionsPage,
+    register_options_page,
+)
 
 
 PLUGIN_NAME = 'Combine Performer Tags'
@@ -41,14 +51,50 @@ PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.html"
 
 PLUGIN_USER_GUIDE_URL = "https://github.com/rdswift/picard-plugins/blob/2.0_RDS_Plugins/plugins/combine_performer_tags/docs/README.md"
 
+OPT_USE_TRACK_RELS = 'track_ars'
+
+OPT_CREDITED_ARTIST = 'cpt_cred_artist'
+OPT_CREDITED_INSTRUMENT = 'cpt_cred_instrument'
+OPT_CREDITED_VOCAL = 'cpt_cred_vocal'
+
+OPT_INSTRUMENT_ATTR_ADDITIONAL = 'cpt_inst_attr_additional'
+OPT_INSTRUMENT_ATTR_GUEST = 'cpt_inst_attr_guest'
+OPT_INSTRUMENT_ATTR_SOLO = 'cpt_inst_attr_solo'
+
+OPT_VOCAL_ATTR_ADDITIONAL = 'cpt_vocal_attr_additional'
+OPT_VOCAL_ATTR_GUEST = 'cpt_vocal_attr_guest'
+OPT_VOCAL_ATTR_SOLO = 'cpt_vocal_attr_solo'
+
+
+class PerformerTags():
+    """Collection of instruments and vocals for a performer.
+    """
+    def __init__(self) -> None:
+        self.instruments = set()
+        self.vocals = set()
+
+    def combine(self) -> list:
+        combined = list(sorted(self.instruments))
+        combined.extend(sorted(self.vocals))
+        return combined
+
 
 def metadata_error(album_id, metadata_element, track_number):
     log.error(f"{PLUGIN_NAME}: {album_id}: Missing '{metadata_element}' in track {track_number} metadata.")
 
 
-def combine_performer_tags(album, album_metadata, track_metadata, release_metadata):     # pylint: disable=unused-argument
+def combine_performer_tags(album, album_metadata, track_metadata, release_metadata):
+    # pylint: disable=unused-argument
+    # pylint: disable=too-many-locals
+    # pylint: disable=too-many-branches
+
+    if not config.setting[OPT_USE_TRACK_RELS]:
+        log.error(f"{PLUGIN_NAME}: Use track relationships is not enabled in Options -> Metadata.")
+        return
+
     album_id = release_metadata['id'] if release_metadata else 'No Album ID'
     track_number = track_metadata['number'] if track_metadata and 'number' in track_metadata else 'No Track Number'
+
     if 'recording' not in track_metadata:
         metadata_error(album_id, 'recording', track_number)
         return
@@ -60,6 +106,18 @@ def combine_performer_tags(album, album_metadata, track_metadata, release_metada
     performers = {}
     performers_tag = []
 
+    cred_artist = config.setting[OPT_CREDITED_ARTIST]
+    cred_instrument = config.setting[OPT_CREDITED_INSTRUMENT]
+    cred_vocal = config.setting[OPT_CREDITED_VOCAL]
+
+    inst_attr_additional = config.setting[OPT_INSTRUMENT_ATTR_ADDITIONAL]
+    inst_attr_guest = config.setting[OPT_INSTRUMENT_ATTR_GUEST]
+    inst_attr_solo = config.setting[OPT_INSTRUMENT_ATTR_SOLO]
+
+    vocal_attr_additional = config.setting[OPT_VOCAL_ATTR_ADDITIONAL]
+    vocal_attr_guest = config.setting[OPT_VOCAL_ATTR_GUEST]
+    vocal_attr_solo = config.setting[OPT_VOCAL_ATTR_SOLO]
+
     for relation in track_metadata['recording']['relations']:
         if (
             'artist' not in relation or not relation['artist']
@@ -68,28 +126,122 @@ def combine_performer_tags(album, album_metadata, track_metadata, release_metada
         ):
             continue
 
-        performer = relation['artist']['name']
+        performer = relation['target-credit'] if cred_artist and relation['target-credit'] else relation['artist']['name']
         instrument = relation['attributes'][0]
 
         # Get as credited name for the instrument or vocal
-        if 'attribute-credits' in relation and instrument in relation['attribute-credits']:
+        if (
+            'attribute-credits' in relation and instrument in relation['attribute-credits']
+            and (
+                    (relation['type'] == 'instrument' and cred_instrument)
+                    or
+                    (relation['type'] == 'vocal' and cred_vocal)
+                )
+        ):
             instrument = relation['attribute-credits'][instrument]
 
         # Add any additional attributes such as 'guest' or 'solo'
         for attr in relation['attributes'][1:]:
+            if (
+                attr == 'additional' and (
+                    (relation['type'] == 'instrument' and not inst_attr_additional)
+                    or
+                    (relation['type'] == 'vocal' and not vocal_attr_additional)
+                )
+            ):
+                continue
+
+            if (
+                attr == 'guest' and (
+                    (relation['type'] == 'instrument' and not inst_attr_guest)
+                    or
+                    (relation['type'] == 'vocal' and not vocal_attr_guest)
+                )
+            ):
+                continue
+
+            if (
+                attr == 'solo' and (
+                    (relation['type'] == 'instrument' and not inst_attr_solo)
+                    or
+                    (relation['type'] == 'vocal' and not vocal_attr_solo)
+                )
+            ):
+                continue
+
             instrument = f"{attr} {instrument}"
 
         if performer not in performers:
-            performers[performer] = set()
+            performers[performer] = PerformerTags()
 
-        performers[performer].add(instrument)
+        if relation['type'] == 'instrument':
+            performers[performer].instruments.add(instrument)
+        elif relation['type'] == 'vocal':
+            performers[performer].vocals.add(instrument)
 
-    for performer, instruments in performers.items():
+    for performer, tags in performers.items():
+        instruments = tags.combine()
         if not instruments:
-            instruments = {'not specified'}
+            instruments = ['not specified']
         performers_tag.append(f"{performer} ({', '.join(instruments)})")
     album_metadata['~performers'] = performers_tag
 
 
+class CombinePerformerTagsOptionsPage(OptionsPage):
+    """Options page for the Combine Performer Tags plugin.
+    """
+
+    NAME = "combine_performer_tags"
+    TITLE = "Combine Performer Tags"
+    PARENT = "plugins"
+
+    options = [
+        config.BoolOption('setting', OPT_CREDITED_ARTIST, True),
+        config.BoolOption('setting', OPT_CREDITED_INSTRUMENT, True),
+        config.BoolOption('setting', OPT_CREDITED_VOCAL, True),
+        config.BoolOption('setting', OPT_INSTRUMENT_ATTR_ADDITIONAL, True),
+        config.BoolOption('setting', OPT_INSTRUMENT_ATTR_GUEST, True),
+        config.BoolOption('setting', OPT_INSTRUMENT_ATTR_SOLO, True),
+        config.BoolOption('setting', OPT_VOCAL_ATTR_ADDITIONAL, True),
+        config.BoolOption('setting', OPT_VOCAL_ATTR_GUEST, True),
+        config.BoolOption('setting', OPT_VOCAL_ATTR_SOLO, True),
+    ]
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_CombinePerformerTagsOptionsPage()
+        self.ui.setupUi(self)
+
+        # Enable external link
+        self.ui.format_description.setOpenExternalLinks(True)
+
+    def load(self):
+        """Load the option settings.
+        """
+        self.ui.cb_credited_artists.setChecked(config.setting[OPT_CREDITED_ARTIST])
+        self.ui.cb_credited_instruments.setChecked(config.setting[OPT_CREDITED_INSTRUMENT])
+        self.ui.cb_credited_vocals.setChecked(config.setting[OPT_CREDITED_VOCAL])
+        self.ui.cb_additional_instruments.setChecked(config.setting[OPT_INSTRUMENT_ATTR_ADDITIONAL])
+        self.ui.cb_guest_instruments.setChecked(config.setting[OPT_INSTRUMENT_ATTR_GUEST])
+        self.ui.cb_solo_instruments.setChecked(config.setting[OPT_INSTRUMENT_ATTR_SOLO])
+        self.ui.cb_additional_vocals.setChecked(config.setting[OPT_VOCAL_ATTR_ADDITIONAL])
+        self.ui.cb_guest_vocals.setChecked(config.setting[OPT_VOCAL_ATTR_GUEST])
+        self.ui.cb_solo_vocals.setChecked(config.setting[OPT_VOCAL_ATTR_SOLO])
+
+    def save(self):
+        """Save the option settings.
+        """
+        config.setting[OPT_CREDITED_ARTIST] = self.ui.cb_credited_artists.isChecked()
+        config.setting[OPT_CREDITED_INSTRUMENT] = self.ui.cb_credited_instruments.isChecked()
+        config.setting[OPT_CREDITED_VOCAL] = self.ui.cb_credited_vocals.isChecked()
+        config.setting[OPT_INSTRUMENT_ATTR_ADDITIONAL] = self.ui.cb_additional_instruments.isChecked()
+        config.setting[OPT_INSTRUMENT_ATTR_GUEST] = self.ui.cb_guest_instruments.isChecked()
+        config.setting[OPT_INSTRUMENT_ATTR_SOLO] = self.ui.cb_solo_instruments.isChecked()
+        config.setting[OPT_VOCAL_ATTR_ADDITIONAL] = self.ui.cb_additional_vocals.isChecked()
+        config.setting[OPT_VOCAL_ATTR_GUEST] = self.ui.cb_guest_vocals.isChecked()
+        config.setting[OPT_VOCAL_ATTR_SOLO] = self.ui.cb_solo_vocals.isChecked()
+
+
 # Register the plugin
 register_track_metadata_processor(combine_performer_tags)
+register_options_page(CombinePerformerTagsOptionsPage)
--- plugins/combine_performer_tags/docs/README.md
+++ plugins/combine_performer_tags/docs/README.md
@@ -2,6 +2,46 @@
 
 ## Overview
 
-This plugin combines all instrument and vocal performer tags into a new multi-value variable `%_performers%` for each track.
+This plugin combines all instrument and vocal performer tags into a new multi-value variable `%_performers%` for each track. It requires that the "***Use track relationships***" setting is enabled in **Options** -> **Metadata**. Each item in the variable is the performer's name followed by the instruments and vocals they performed, for example "*Jackson Browne (acoustic guitar, piano, lead vocals)*".
 
-The plugin makes no additional calls to the MusicBrainz database, and it does not remove any of the `%performer:*` tags.
+The plugin makes no additional calls to the MusicBrainz database, and it does not remove any of the `%performer:*%` tags.
+
+---
+
+## Option Settings
+
+The plugin adds a settings page under the "Plugins" section under "Options..." from Picard's main menu.  This allows you to control how the plugin operates with respect to processing track performance artists and the attribute details included in `%_performers%` variable.
+
+![Combine Performer Tags Option Settings](cpt_option_settings.png "Combine Performer Tags Option Settings")
+
+---
+
+## What it Does
+
+This plugin reads the track metadata provided to Picard, extracts the list of associated instrument and vocal performers, and combines the information in a multi-value variable for use in Picard scripts.
+
+---
+
+## Example Usage
+
+You can use the following tagger script to include this in the tags written to the files:
+
+```taggerscript
+$noop( Set as a multi-value tag. )
+$setmulti(Performers,%_performers%)
+```
+
+or:
+
+```taggerscript
+$noop( Set as a regular text tag. )
+$set(Performers,%_performers%)
+```
+
+If you have included this as a combined tag, you might also want to remove the individual `%performer:*%` tags, which can be accomplished by the tagger script:
+
+```taggerscript
+$unset(performer:*)
+```
+
+---
